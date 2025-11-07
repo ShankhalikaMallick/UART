88e038dfb59610f8fbd40958893023cf5685b014
Shankhalika <88712498+ShankhalikaMallick@users.noreply.github.com>
2025-11-07 10:12:07 +0530

    Create rx_detect_start.v

---
+++ rx_detect_start.v
@@ -0,0 +1,28 @@
+module rx_detect_start(
+    input clk,
+    input rx_datain,
+    output de_strtbit );
+
+    always @ (posedge clk)
+    begin
+        if (rx_datain==0)
+        begin
+            if (count == 4'd15)
+            begin
+                count=0;
+                de_strtbit=1;               
+            end
+            else
+            begin
+                count= count+1;
+                de_strtbit=0;
+            end
+        end
+        else
+        begin
+            count = 0;
+            de_strtbit =0;                 
+        end
+    end
+    
+endmodule
