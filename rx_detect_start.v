ad2b3dadebae11ac8c78cfbdbec9772b8cc99401
Shankhalika <88712498+ShankhalikaMallick@users.noreply.github.com>
2025-11-11 20:19:45 +0530

    rx_detect_start
    Refactored rx_detect_start module to simplify start bit detection.

--- rx_detect_start.v
+++ rx_detect_start.v
@@ -1,30 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11.11.2025 16:16:47
+// Design Name: 
+// Module Name: rx_detect_start
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module rx_detect_start(
-    input clk,
-    input rx_datain,
-    output de_strtbit );
+    input rx_in,
+    output strt_bit );
 
-    reg [3:0] count=0;
-
-    always @ (posedge clk)
-    begin
-        if (rx_datain==0)
-        begin
-            if (count == 4'd15)
-            begin
-                count=0;
-                de_strtbit=1;               
-            end
-            else
-            begin
-                count= count+1;
-                de_strtbit=0;
-            end
-        end
-        else
-        begin
-            count = 0;
-            de_strtbit =0;                 
-        end
-    end
+    assign strt_bit= !(rx_in);
+    // if input is 0 then start
     
 endmodule
