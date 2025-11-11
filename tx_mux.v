d350f08354b33652df109b83d33e6c5e7678698f
Shankhalika <88712498+ShankhalikaMallick@users.noreply.github.com>
2025-11-11 20:37:21 +0530

    tx_mux
    Refactor tx_mux module to use a case statement for output selection.

--- tx_mux.v
+++ tx_mux.v
@@ -1,14 +1,39 @@
-module tx_mux(
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11.11.2025 16:21:13
+// Design Name: 
+// Module Name: tx_mux
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tx_mux (
+    input [1:0] select,
     input data_bit,
     input parity_bit,
-    input s0,
-    input s1,
-    output tx_packet);
+    output reg tx_out );
 
-    wire start_bit;
-    wire stop_bit;
-    assign start_bit = 1'b0;
-    assign stop_bit = 1'b1;
-    // assigning multiplexer output
-    assign tx_packet= (~s0 & ~s1 & start_bit) | (~s1 & s0 & data_bit) | (s1 & ~s0 & parity_bit) | (s1 & s0 & stop_bit); 
+    always @(select, data_bit, parity_bit)
+    begin
+        case (select)
+            2'b00: tx_out=1'b0;
+            2'b01: tx_out=data_bit;
+            2'b10: tx_out=parity_bit;
+            2'b11: tx_out=1'b1;
+        endcase
+    end
+
 endmodule
