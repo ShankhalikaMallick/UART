cbba1cd7c67cae660373f155dcc190b47b34991c
Shankhalika <88712498+ShankhalikaMallick@users.noreply.github.com>
2026-01-14 09:05:50 +0530

    Refactor brg module and testbench structure
    Updated the module definition and added clock generation logic.

--- brg_tb.v
+++ brg_tb.v
@@ -1,31 +1,78 @@
-`timescale 1ps/1ps
-`include "brg.v"
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11.11.2025 17:22:04
+// Design Name: 
+// Module Name: brg
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
-module tb_uart_baud_gen;
+module brg(
+    input  clk_in,
+    input  reset,
+    output reg txclk,
+    output reg rxclk
+); 
+    /* 
+    system clock frequency = 16 MHz
+    transmitter clock frequency at 9600 baud rate = 16000000/9600 ≈ 1667 MHz
+    receiver must have oversampling of 16X
+    receiver clock frequency at 9600*16 baud rate = 16000000/(9600*16) ≈ 104 MHz
+    */
+    
+    // Baud rate parameters
+    localparam integer BAUD     = 9600;
+    localparam integer FREQ     = 1000000;
+    localparam integer OVS      = 16;
 
-    // Testbench signals
-    reg clk;
-    reg reset;
-    wire tx_tick;
-    wire rx_tick;
+    // Divisors
+    localparam integer TX_DIV   = 1667;
+    localparam integer RX_DIV   = 104;
 
-    // Instantiate the DUT
-    brg dut ( clk, reset, tx_tick, rx_tick);
-    // --------------------------------------------------
-    // 16 MHz clock generation
-    // Period = 62.5 ns
-    // --------------------------------------------------
-    initial begin
-        clk = 0;
-        reset=1'b1;
-        #1; reset=1'b0;
-        $monitor("%b, %b, %b, %b",clk, reset,tx_tick, rx_tick);
-        #20000;
-        #20000;
-        #20000;
-        #20000; $finish;
+    reg [11:0] txcount = 0;    // 1667 is of 11 bits in binary
+    reg [6:0] rxcount = 0;     // 104 is of 7 bits in binary
+
+    always @(posedge clk_in or posedge reset) begin
+        if (reset) begin
+            txcount <= 0;
+            rxcount <= 0;
+            txclk    <= 0;
+            rxclk    <= 0;
+        end 
+        else 
+        begin
+            //---------- RX COUNTER ----------
+            if (rxcount == RX_DIV-1) begin
+                rxcount <= 0;
+                rxclk   <= 1'b1;
+            end else begin
+                rxcount <= rxcount + 1'b1;
+                rxclk    <= 0;
+            end
+
+            //---------- TX COUNTER ----------
+            
+            if (txcount == TX_DIV-1) begin
+                txcount <= 0;
+                txclk   <= 1'b1;       // txclk becomes 1 this cycle , becomes 0 in the next cycle
+            end else begin
+                txcount <= txcount + 1'b1;
+                txclk   <= 0;
+            end
+            
+
+        end
     end
-    always 
-    #0.001 clk <= !clk;
-
 endmodule
