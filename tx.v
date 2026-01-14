2a202fe159bbea2fa36d32351630a8f81e7c37ee
Shankhalika <88712498+ShankhalikaMallick@users.noreply.github.com>
2026-01-14 08:59:21 +0530

    Update and rename uart_tx.v to tx.v

--- tx.v
+++ tx.v
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps
+`timescale 1ps / 1ps 
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
@@ -29,20 +29,19 @@ module uart_tx(
     input clk,
     input reset,
     input tx_start,
-    input [7:0] tx_data_in, 
-    output tx_data_out,
-    output tx_busy );
+    input [7:0] tx_data_in,                 // input data of 8 bits
+    output tx_data_out);                    // packet of 11 bits given as output including start , data, parity and stop bits 
 
-    wire parity_out;
-    wire piso_op;
+    wire parity_bit;
+    wire data_bit;
     wire [1:0] sel;
     wire piso_load;
-    wire piso_shift;
     wire parity_load;
+    wire piso_shift;
 
-    tx_mux tx1 ( sel, piso_op, parity_out, tx_data_out );
-    tx_parity tx2 (clk, reset, parity_load, tx_data_in );
-    tx_piso tx3 (clk, reset, piso_load, piso_shift, tx_data_in, piso_op );
-    tx_fsm tx4 ( clk, reset, tx_start, sel, piso_load, piso_shift, parity_load, tx_busy); 
+    tx_mux tx1 ( sel, data_bit, parity_bit, tx_data_out );
+    tx_parity tx2 (clk, reset, tx_data_in, parity_load, parity_bit );
+    tx_piso tx3 (clk, reset, piso_load, piso_shift, tx_data_in, data_bit );
+    tx_fsm tx4 ( clk, reset,tx_start, sel, piso_load, piso_shift, parity_load); 
 
 endmodule
