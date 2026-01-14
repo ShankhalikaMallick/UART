d477076ea71d058b92bf308ac00444a9a74c6676
Shankhalika <88712498+ShankhalikaMallick@users.noreply.github.com>
2026-01-14 08:55:33 +0530

    Refactor tx_mux module timing and case statement
    Updated the timing scale and improved the case statement for clarity.

--- tx_mux.v
+++ tx_mux.v
@@ -1,24 +1,4 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11.11.2025 16:21:13
-// Design Name: 
-// Module Name: tx_mux
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
+`timescale 1ps/1ps
 
 module tx_mux (
     input [1:0] select,
@@ -26,13 +6,14 @@ module tx_mux (
     input parity_bit,
     output reg tx_out );
 
-    always @(select, data_bit, parity_bit)
+    always @(*)
     begin
         case (select)
-            2'b00: tx_out=1'b0;
-            2'b01: tx_out=data_bit;
-            2'b10: tx_out=parity_bit;
-            2'b11: tx_out=1'b1;
+            2'b00: tx_out=1'b0;             // start bit
+            2'b01: tx_out=data_bit;         // data from piso 
+            2'b10: tx_out=parity_bit;       // parity bit
+            2'b11: tx_out=1'b1;             // stop bit
+            default: tx_out = 1'b1;   // IDLE line high
         endcase
     end
 
